962f898f7300d5c33a2e1768df2276141493c1b2
QCrumedy <itstutt09@gmail.com>
2025-09-04 14:50:26 +0000

    saving debug exercise

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,68 @@
-$(document).ready(function () {
+$(document).ready(function(){
   // Your code goes here
+$("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", 50)
+  .css("left", 50)
+  .appendTo("#die");
 
-});
+
+  function makeDot(top,left,elementID){
+    $("<div>")
+  .css("height", 15)
+  .css("width", 15)
+  .css("background-color", "black")
+  .css("position", "absolute")
+  .css("top", top)
+  .css("left", left)
+  .appendTo(elementID);
+  }
+
+
+  
+  function rollDie(){
+var randomNum = Math.ceil(Math.random() * 6);
+console.log(randomNum);
+
+ $("#die").empty();
+    if (randomNum === 1) {
+  makeDot(50, 50, "#die"); // middle middle
+} else if (randomNum === 2) {
+  makeDot(25, 25, "#die"); // top left
+  makeDot(75, 75, "#die"); // bottom right
+} else if (randomNum === 3) {
+  makeDot(25, 25, "#die"); // top left
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(50, 50, "#die"); // middle middle
+} else if (randomNum === 4) {
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+} else if (randomNum === 5) {
+  makeDot(50, 50, "#die"); // middle middle
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+
+}else if(randomNum === 6) {
+  makeDot(75, 75, "#die"); // bottom right
+  makeDot(25, 25, "#die"); // top left
+  makeDot(25, 75, "#die"); // bottom left
+  makeDot(75, 25, "#die"); // top right
+  makeDot(50,75, "#die")
+  makeDot(50,25,"#die")
+}   
+  } 
+
+ $("#die").on("click", rollDie);
+console.log("clicked");
+
+  
+  
+ 
+})
