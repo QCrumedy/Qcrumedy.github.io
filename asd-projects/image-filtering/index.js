7063e593ad3a9ff23017c6ceee37033088bef2b6
QCrumedy <itstutt09@gmail.com>
2025-10-23 14:22:51 +0000

    completed image filtering project

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,11 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  applyFilter(reddify);
+applyFilterNoBackground(decreaseBlue);
+applyFilter(increaseGreenByBlue);
+applyFilterNoBackground(reddify);
 
-  
 
   // do not change the below line of code
   render($("#display"), image);
@@ -34,16 +37,70 @@ function applyAndRender() {
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
 
 
+
+function applyFilter(filterFunction) {
+for (let i = 0; i < image.length; i++) {
+
+  for (let j = 0; j < image[i].length; j++) {
+    let pixel = image[i][j];
+    let pixelArray = rgbStringToArray(pixel);
+    filterFunction(pixelArray)
+    let updatedPixel = rgbArrayToString(pixelArray);
+    image[i][j] = updatedPixel;
+  }
+}
+}
+
 // TODO 9 Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+
+  var backgroundColor = image[0][0];
+
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+
+      
+      if (image[i][j] !== backgroundColor) {
+
+       
+        var pixelArray = rgbStringToArray(image[i][j]);
+        filterFunction(pixelArray);
+        var filteredPixel = rgbArrayToString(pixelArray);
+        image[i][j] = filteredPixel;
+      }
+    }
+  }
+}
 
 
 // TODO 6: Create the keepInBounds function
-
+function keepInBounds(number){
+  if(number<0){
+    return 0;
+  }
+  if(number>255){
+    return 255;
+  }
+  return number;
+}
 
 // TODO 4: Create reddify filter function
 
-
+function reddify(pixelArray) {
+  return pixelArray[RED] = 200;
+}
 // TODO 7 & 8: Create more filter functions
-
-
+function decreaseBlue(pixelArray){
+ for(let i=0;i<pixelArray.length;i++){
+  let pixel=pixelArray[i];
+  pixel[BLUE]=keepInBounds(pixel[BLUE]-50,0,255);
+ }
+}
+function increaseGreenByBlue(pixels){
+  const greenValue=pixels[GREEN];
+  const blueValue=pixels[BLUE];
+  const newGreenValue=greenValue+blueValue;
+  const clampedGreenValue=keepInBounds(newGreenValue);
+  pixels[GREEN]=clampedGreenValue;}
+ 
 // CHALLENGE code goes below here
